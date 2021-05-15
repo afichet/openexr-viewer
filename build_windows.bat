d9244ccc6ebb2cf8a7dd7e808607c73a5db398ca
Alban Fichet <alban.fichet@gmx.fr>
2021-05-15 22:38:49 +0100

    Windows package generation

--- build_windows.bat
+++ build_windows.bat
@@ -5,9 +5,10 @@ cmake .. ^
   -DZLIB_ROOT="%~dp0\build\depends\lib"^
   -DImath_DIR="%~dp0\build\depends\lib\lib\cmake\Imath"^
   -DOpenEXR_DIR="%~dp0\build\depends\lib\lib\cmake\OpenEXR"^
-  -DCMAKE_INSTALL_PREFIX="%~dp0\build\install"
+  -DCMAKE_INSTALL_PREFIX="%~dp0\build\install"^
+  -DCMAKE_BUILD_TYPE="Release"^
+  -DCMAKE_CONFIGURATION_TYPES="Release"
 
 cmake --build . --config Release
 
-xcopy .\depends\lib\bin\*.dll Release
-C:\Qt\6.1.0\msvc2019_64\bin\windeployqt Release/openexr-viewer.exe
+cpack
