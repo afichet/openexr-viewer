f4004d2f9b914418e6db3b4d371463d2c0828d52
Alban Fichet <alban.fichet@gmx.fr>
2022-01-16 05:44:40 +0100

    Incorect license in About window

--- src/view/about.cpp
+++ src/view/about.cpp
@@ -8,35 +8,47 @@ About::About(QWidget *parent): QDialog(parent), ui(new Ui::About)
     ui->textBrowser->setHtml(
       "<h1>OpenEXR Viewer</h1>"
       "<p><em>"
-      + tr("Version ") + QApplication::applicationVersion() + "</em></p>"
-      + tr(
-        "<h2>Author</h2>                                                        \
-                    </p>Alban Fichet &lt;alban.fichet@gmx.fr&gt;</p>                              \
-                                                                                            \
-                    <h2>Licence GPLv3.</h2>                                                 \
-                    <p><strong>Copyright (c) Alban Fichet 2021.</strong></p>                \
-                    <p>                                                                     \
-                    This program is free software: you can redistribute it and/or modify    \
-                    it under the terms of the GNU General Public License as published by    \
-                    the Free Software Foundation, either version 3 of the License, or       \
-                    (at your option) any later version.</p>                                 \
-                    <p>                                                                     \
-                    This program is distributed in the hope that it will be useful,         \
-                    but WITHOUT ANY WARRANTY; without even the implied warranty of          \
-                    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           \
-                    GNU General Public License for more details.</p>                        \
-                    <p>                                                                     \
-                    You should have received a copy of the GNU General Public License       \
-                    along with this program.  If not, see <https://www.gnu.org/licenses/>.  \
-                    </p>                                                                    \
-                                                                                            \
-                    <h2>Icons</h2>                                                          \
-                    <div>Icons made by                                                      \
-                    <a href=\"https://www.flaticon.com/authors/dinosoftlabs\"               \
-                       title=\"DinosoftLabs\">DinosoftLabs</a> from                         \
-                    <a href=\"https://www.flaticon.com/\"                                   \
-                       title=\"Flaticon\">www.flaticon.com</a></div>                        \
-                    "));
+      //TODO + tr("Version ") + QApplication::applicationVersion() + "</em></p>"
+      + tr(" \
+        BSD 3-Clause License \
+        \
+        <p>Copyright (c) 2021 - 2022, Alban Fichet <br/> \
+        All rights reserved. </p>\
+        \
+        <p>Redistribution and use in source and binary forms, with or without \
+        modification, are permitted provided that the following conditions are \
+        met:</p> \
+        <ol> \
+        <li>Redistributions of source code must retain the above copyright \
+        notice, this list of conditions and the following disclaimer.</li> \
+        \
+        <li>Redistributions in binary form must reproduce the above copyright \
+        notice, this list of conditions and the following disclaimer in the \
+        documentation and/or other materials provided with the distribution.</li> \
+        \
+        <li>Neither the name of the copyright holder nor the names of its \
+        contributors may be used to endorse or promote products derived from \
+        this software without specific prior written permission.</li> \
+        </ol>\
+        <p>THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \
+        \"AS IS\" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT \
+        LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR \
+        A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT \
+        HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, \
+        SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT \
+        LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, \
+        DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY \
+        THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT \
+        (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE \
+        OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.</p> \
+        \
+        <h2>Icons</h2>                                                          \
+        <div>Icons made by                                                      \
+        <a href=\"https://www.flaticon.com/authors/dinosoftlabs\"               \
+           title=\"DinosoftLabs\">DinosoftLabs</a> from                         \
+        <a href=\"https://www.flaticon.com/\"                                   \
+           title=\"Flaticon\">www.flaticon.com</a></div>                        \
+        "));
 }
 
 About::~About()
